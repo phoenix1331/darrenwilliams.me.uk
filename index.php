675040e23c27104265baac8406d61c85e648444b
Darren Williams <design@phoenix1331.co.uk>
2015-03-20 21:21:01 +0000

    nav edits

--- index.php
+++ index.php
@@ -11,31 +11,32 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
+          <a class="navbar-brand" href="#">Darren Williams</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
             <li><a href="../navbar/">About</a></li>
             <li><a href="../navbar-static-top/">Portfolio</a></li>
             <li><a href="../navbar-static-top/">CV</a></li>
+            <li><a href="../navbar-static-top/">Contact</a></li>
             <!--<li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>-->
           </ul>
         </div><!--/.nav-collapse -->
       </div>
     </nav>
-
+    
+      <video id="hero" poster="http://placehold.it/350x150" loop autoplay muted >
+       <!-- <source src="video/vid2.mp4" type="video/mp4">-->
+      </video>
     <div class="container">
 
-      <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
-        <h1 class="center">Navbar example</h1>
+        <!--<h1 class="center">Navbar example</h1>
         <p class="center">This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
         <p class="center">To see the difference between static and fixed top navbars, just scroll.</p>
         <p class="center">
           <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-        </p>
-        <video id="hero" poster="http://placehold.it/350x150" loop autoplay muted >
-        <source src="video/vid2.mp4" type="video/mp4"></video>
-
+        </p>-->
       </div>
 
     </div> <!-- /container -->
@@ -90,41 +91,5 @@
       </div>
     </div>
     </div><!--close main-->
-    <footer>
-     <div class="container center">
-       <div class="row" data-sr="enter left">
-            <div class="col-lg-4">
-              <img src="http://placehold.it/350x150">
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-            <div class="col-lg-4">
-              <img src="http://placehold.it/350x150">
-              <h2>Heading</h2>
-              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-            <div class="col-lg-4">
-              <img src="images/misyb_port.png">
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-            </div><!-- /.col-lg-4 -->
-        </div>  
-        </div>    
-    </footer>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/scrollReveal.js"></script>
-    <script>
-      window.sr = new scrollReveal();
-    </script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+<?php include('includes/footer.php'); ?>
